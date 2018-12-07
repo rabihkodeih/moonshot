6a69118a731e6182559b0dbec7814c7bb0c20820
Rabih Kodeih <rabih.kodeih@gmail.com>
2018-12-07 16:37:14 +0200

    Add main UI scaffold

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Created on Dec 3, 2018
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk
 
 
 class MainWindow(Gtk.Window):
@@ -16,33 +16,81 @@ class MainWindow(Gtk.Window):
         self.set_border_width(10)
         self.set_default_size(400, 600)
         #TODO: set fixes size, do not resize
-        self.header_bar = self.create_header_bar()
-        self.set_titlebar(self.header_bar)
+        # header bar
+        self.set_titlebar(self.create_header_bar())
+        # main container
+        main_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(main_container)
+        # location selector
+        location_selector = self.create_location_selector()
+        main_container.pack_start(location_selector, True, True, 0)
+        # todays date box
+        todays_date_box = self.create_todays_date_box()
+        main_container.pack_start(todays_date_box, True, True, 0)
+        # weather point box
+        weather_point_box = self.create_weather_point_box()
+        main_container.pack_start(weather_point_box, True, True, 0)
+        # weather day box
+        weather_day_box = self.create_weather_day_box()
+        main_container.pack_start(weather_day_box, True, True, 0)
+        # weather week box
+        weather_week_box = self.create_weather_week_box()
+        main_container.pack_start(weather_week_box, True, True, 0)
+        
 
     def create_header_bar(self):
         hb = Gtk.HeaderBar()
         hb.set_decoration_layout("menu:close")
         hb.set_show_close_button(True)
         hb.props.title = "Moonshot"
-        
         # refresh button
         icon = Gio.ThemedIcon(name="view-refresh-symbolic")
         refresh_btn = Gtk.Button(None, image=Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON))
         hb.pack_start(refresh_btn)
-
         # temperature chart button
         icon = Gio.ThemedIcon(name="utilities-system-monitor-symbolic")
         tmpchart_btn = Gtk.Button(None, image=Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON))
         hb.pack_start(tmpchart_btn)
-
         # settings button
         icon = Gio.ThemedIcon(name="emblem-system-symbolic")
         settings_btn = Gtk.Button(None, image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON))
         hb.pack_start(settings_btn)
-        
         return hb
 
-        
+    def create_todays_date_box(self):
+        box = Gtk.Box()
+        box.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1.0, 0.8, 0.8, 1.0))
+        label = Gtk.Label('todays_date_box') 
+        box.pack_start(label, True, True, 0)
+        return box
+
+    def create_location_selector(self):
+        box = Gtk.Box()
+        box.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(0.8, 1.0, 0.8, 1.0))
+        label = Gtk.Label('location_selector') 
+        box.pack_start(label, True, True, 0)
+        return box
+    
+    def create_weather_point_box(self):
+        box = Gtk.Box()
+        box.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(0.8, 0.8, 1.0, 1.0))
+        label = Gtk.Label('weather_point_box') 
+        box.pack_start(label, True, True, 0)
+        return box
+    
+    def create_weather_day_box(self):
+        box = Gtk.Box()
+        box.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1.0, 1.0, 0.8, 1.0))
+        label = Gtk.Label('weather_day_box') 
+        box.pack_start(label, True, True, 0)
+        return box
+
+    def create_weather_week_box(self):
+        box = Gtk.Box()
+        box.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1.0, 0.8, 1.0, 1.0))
+        label = Gtk.Label('weather_week_box') 
+        box.pack_start(label, True, True, 0)
+        return box
 
       
 def launch_main_window():
